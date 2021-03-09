c5742aa84de033bd2c563e6953137614ee9be255
surabhy <59991626+surabhy@users.noreply.github.com>
2021-03-09 12:35:39 +0530

    Update S3.cmd

--- S3.cmd
+++ S3.cmd
@@ -1,7 +1,7 @@
 cd <copy the path where policy.json file placed in your computer>
 aws configure set AWS_ACCESS_KEY_ID <Your Access Key ID>
 aws configure set AWS_SECRET_ACCESS_KEY <your AWS Secret Access Key>
-aws configure set default.region ap-south-1
+aws configure set default.region eu-west-1
 aws s3api create-bucket --bucket <Bucket Name> --region eu-west-1  --create-bucket-configuration LocationConstraint=eu-west-1
 aws s3 website s3://<Bucket Name> --index-document index.html
 aws s3api put-bucket-policy --bucket <bucket Name> --policy file://policy.json
