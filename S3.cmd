8f80ee27b410a6685c4f7142b39b67e623791c74
surabhy <59991626+surabhy@users.noreply.github.com>
2021-03-09 12:34:25 +0530

    Update S3.cmd

--- S3.cmd
+++ S3.cmd
@@ -1,12 +1,12 @@
-cd C:\Users\surabhy.mr\Desktop\Test
-aws configure set AWS_ACCESS_KEY_ID AKIAR3OMUZAFQSODH5PC
-aws configure set AWS_SECRET_ACCESS_KEY +9xq0fh41QI41hjPTXdfWqfTrjM+8V8MrgF/PpjE
+cd <copy the path where policy.json file placed in your computer>
+aws configure set AWS_ACCESS_KEY_ID <Your Access Key ID>
+aws configure set AWS_SECRET_ACCESS_KEY < Secret Access Key>
 aws configure set default.region ap-south-1
-aws s3api create-bucket --bucket surabhy123 --region eu-west-1  --create-bucket-configuration LocationConstraint=eu-west-1
-aws s3 website s3://surabhy123 --index-document index.html
-aws s3api put-bucket-policy --bucket surabhy123 --policy file://policy.json
+aws s3api create-bucket --bucket <Bucket Name> --region eu-west-1  --create-bucket-configuration LocationConstraint=eu-west-1
+aws s3 website s3://<Bucket Name> --index-document index.html
+aws s3api put-bucket-policy --bucket <bucket Name> --policy file://policy.json
 echo "<html><center><h1>Hello World</h1></center></html>" > index.html
-aws s3 cp index.html s3://surabhy123
+aws s3 cp index.html s3://<Bucket Name>
 
 
 
