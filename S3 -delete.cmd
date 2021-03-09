fda0886e1cd7f6423ae2460f8c45774da702de59
surabhy <59991626+surabhy@users.noreply.github.com>
2021-03-09 12:46:19 +0530

    Update S3 -delete.cmd

--- S3 -delete.cmd
+++ S3 -delete.cmd
@@ -1,8 +1,7 @@
-cd C:\Users\surabhy.mr\Desktop\TallyGo
-aws configure set AWS_ACCESS_KEY_ID AKIAR3OMUZAFQSODH5PC
-aws configure set AWS_SECRET_ACCESS_KEY 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
-aws configure set default.region ap-south-1
-aws s3 rb s3://surabhy123 --force
+aws configure set <AWS_ACCESS_KEY_ID>
+aws configure set <AWS_SECRET_ACCESS_KEY >
+aws configure set default.region eu-west-1
+aws s3 rb s3://<Bucket name> --force
 
 
 
